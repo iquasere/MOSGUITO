dffbc47bab716f4d733ce8ca9318016c008a0452
António Dias <adias@mosano.eu>
2020-11-09 23:29:48 +0000

    Implment Accordion component. Bind kegg data.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import LabelledCheckbox from "./components/labelledCheckbox"
 import LabelledSelect from "./components/labelledSelect"
 import { defaultValues } from './utils/defaultValues'
 import { keggMaps } from './utils/keggMaps'
+import download from './utils/download'
 import {
   assemblerOptions,
   errorModelOptions,
@@ -20,10 +21,9 @@ import {
   keggcharterTaxaLevelOptions
 } from './utils/options'
 import './App.css'
-import KeggcharterAccordion from "./components/keggcharterAccordion";
+import KeggCharter from './components/KeggCharter'
 
 const Main = () => {
-
   const [values, setValues] = useState(defaultValues)
 
   console.log(values)
@@ -39,73 +39,8 @@ const Main = () => {
 
     console.log(JSON.stringify(values, null, 2).toString())
     console.log(YAML.stringify(values))
-  }
 
-  const bacon = 'Spicy jalapeno bacon ipsum dolor amet spare ribs picanha'
-
-  const ToggledSection = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggleIsOpen = () => setIsOpen(!isOpen)
-
-    return (
-      <section>
-        <div>
-          <input
-            type='checkbox'
-            checked={isOpen}
-            onChange={toggleIsOpen}
-          />
-          <span>Toggle me senpai</span>
-        </div>
-        {
-          isOpen && (
-            <p>
-              {`${bacon} ${bacon} ${bacon}`}
-            </p>
-          )
-        }
-      </section>
-    )
-  }
-
-  function AssemblerOrErrorModel(props){
-    if (props.doAssembly) {
-      return <LabelledSelect
-        label='Choose assembler'
-        value={values.assembler}
-        onChange={(ev) => handleChange('assembler', ev.target.value)}
-        options={assemblerOptions}
-        />
-    }
-    return <LabelledSelect
-        label='Choose error model'
-        value={values.errorModel}
-        onChange={(ev) => handleChange('errorModel', ev.target.value)}
-        options={errorModelOptions}
-        />
-  }
-
-  // Function to download data to a file
-  function Download(data, filename, type) {
-    var file = new Blob([data], {type: type});
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    else { // Others
-        var a = document.createElement("a"),
-                url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function() {
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);
-        }, 0);
-    }
-}
-
-    function DownloadJson() {
-    JSON.stringify(values, null, 2).toString()
+    download(JSON.stringify(values, null, 2), 'config.json', 'json')
   }
 
   return (
@@ -132,21 +67,37 @@ const Main = () => {
               label='Number of threads to use'
               value={values.threads}
               onChange={(ev) => handleChange('threads', ev.target.value)}
-              />
-
-            <LabelledCheckbox
-                label='Perform assembly'
-                checked={values.doAssembly}
-                setChecked={(ev) => handleChange('doAssembly', ev.target.checked)}
             />
 
-            <AssemblerOrErrorModel doAssembly={values.doAssembly} />
+            <LabelledCheckbox
+              label='Perform assembly'
+              checked={values.doAssembly}
+              setChecked={(ev) => handleChange('doAssembly', ev.target.checked)}
+            />
+
+            {
+              values.doAssembly ? (
+                <LabelledSelect
+                  label='Choose assembler'
+                  value={values.assembler}
+                  onChange={(ev) => handleChange('assembler', ev.target.value)}
+                  options={assemblerOptions}
+                />
+              ) : (
+                  <LabelledSelect
+                    label='Choose error model'
+                    value={values.errorModel}
+                    onChange={(ev) => handleChange('errorModel', ev.target.value)}
+                    options={errorModelOptions}
+                  />
+                )
+            }
 
             <LabelledSelect
-                label='Choose markerset'
-                value={values.markerset}
-                onChange={(ev) => handleChange('markerset', ev.target.value)}
-                options={markersetOptions}
+              label='Choose markerset'
+              value={values.markerset}
+              onChange={(ev) => handleChange('markerset', ev.target.value)}
+              options={markersetOptions}
             />
 
             <LabelledTextField
@@ -157,9 +108,9 @@ const Main = () => {
             />
 
             <LabelledCheckbox
-                label='Download UniProt'
-                checked={values.downloadUniprot}
-                setChecked={(ev) => handleChange('downloadUniprot', ev.target.checked)}
+              label='Download UniProt'
+              checked={values.downloadUniprot}
+              setChecked={(ev) => handleChange('downloadUniprot', ev.target.checked)}
             />
 
             <LabelledNumberField
@@ -169,17 +120,17 @@ const Main = () => {
             />
 
             <LabelledSelect
-                label='Choose normalization method'
-                value={values.normalizationMethod}
-                onChange={(ev) => handleChange('normalizationMethod', ev.target.value)}
-                options={normalizationMethodOptions}
+              label='Choose normalization method'
+              value={values.normalizationMethod}
+              onChange={(ev) => handleChange('normalizationMethod', ev.target.value)}
+              options={normalizationMethodOptions}
             />
 
             <LabelledSelect
-                label='Choose the taxonomic level to represent with KEGGCharter'
-                value={values.keggcharterTaxaLevel}
-                onChange={(ev) => handleChange('keggcharterTaxaLevel', ev.target.value)}
-                options={keggcharterTaxaLevelOptions}
+              label='Choose the taxonomic level to represent with KEGGCharter'
+              value={values.keggcharterTaxaLevel}
+              onChange={(ev) => handleChange('keggcharterTaxaLevel', ev.target.value)}
+              options={keggcharterTaxaLevelOptions}
             />
 
             <LabelledNumberField
@@ -188,9 +139,10 @@ const Main = () => {
               onChange={(ev) => handleChange('keggcharterNumberOfTaxa', ev.target.value)}
             />
 
-            <KeggcharterAccordion
-                maps={keggMaps}
-                mapsList={values.keggcharterMaps}
+            <KeggCharter
+              maps={keggMaps}
+              keggMapList={values.keggcharterMaps}
+              onChange={(value) => handleChange('keggcharterMaps', value)}
             />
 
           </CardContent>
@@ -204,7 +156,6 @@ const Main = () => {
               type='submit'
               variant='contained'
               color='secondary'
-              onClick={(ev) => Download(JSON.stringify(values, null, 2), 'config.json', 'json')}
             >
               Download configuration file
             </Button>
