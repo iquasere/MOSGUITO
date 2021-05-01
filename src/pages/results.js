787ee005439c6c8edf5467aba7f4be81b05e4a6b
José Pereira <74066223+JosePereira97@users.noreply.github.com>
2021-05-01 15:49:14 +0100

    x

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import {Button, CardActions} from "@material-ui/core";
+
 
 import {DashboardLayout} from '../components/Layout';
 
 
-const ResultsPage = () => { //inserir o botão responsável por modstrar a página dos results
+const ResultsPage = () => { //inserir o botão responsável por modstrar a página dos result
   return (
     <DashboardLayout> 
-      <h1 style={{textAlign: 'center'}} >Upload your Results! No Results avaiable!</h1>
-      <Button
-      href = 'https://www.youtube.com'
-      target = '_blank'
-      color = 'secondary'
-      variant = 'contained'
-      >
-        Upload
-      </Button>
+      <h3>Choose the local(s) zip file(s)</h3>
+      <p className="note">Note : your browser will process the zip file, don't choose a file too big !</p>
+      <input type="file" id="file" name="file" multiple /><br />
     </DashboardLayout>
-  )
-}
+  )}
 
 export default ResultsPage;
