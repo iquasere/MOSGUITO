71e775f9cd953e31b5f51d56ed0a7d78dcbff977
JosePereira97 <74066223+JosePereira97@users.noreply.github.com>
2021-05-01 13:04:24 +0100

    g

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Button, CardActions} from "@material-ui/core";
 
 import {DashboardLayout} from '../components/Layout';
 
-const ResultsPage = () => {
+
+const ResultsPage = () => { //inserir o botão responsável por modstrar a página dos results
   return (
-    <DashboardLayout>
-      <h2>Results Page</h2>
+    <DashboardLayout> 
+      <h1 style={{textAlign: 'center'}} >Upload your Results! No Results avaiable!</h1>
+      <Button
+      href = 'https://www.youtube.com'
+      target = '_blank'
+      color = 'secondary'
+      variant = 'contained'
+      >
+        Upload
+      </Button>
     </DashboardLayout>
   )
 }
