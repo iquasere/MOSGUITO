070920454b7450bfd111600eed613405e54d38cf
JosePereira97 <74066223+JosePereira97@users.noreply.github.com>
2021-05-10 12:47:57 +0100

    unzip implementation

--- src/pages/ServerUnzip.js
+++ src/pages/ServerUnzip.js
@@ -1,5 +1,8 @@
-import React from 'react'
-import 'jszip'
+import React from 'react';
+import * as zip from "@zip.js/zip.js";
+// create a BlobReader to read with a ZipReader the zip from a Blob object
+export const reader = new zip.ZipReader();
+
 
 
 
--- src/pages/results.js
+++ src/pages/results.js
@@ -1,42 +1,69 @@
 import React, { useState } from 'react';
-import {DashboardLayout} from '../components/Layout';
-import {Button, Toolbar, Typography} from "@material-ui/core";
+import { DashboardLayout } from '../components/Layout';
+import { Button, Toolbar, Typography } from "@material-ui/core";
 import ReactHtmlParser from 'react-html-parser';
-import { file } from 'jszip';
+import * as zip from "@zip.js/zip.js/dist/zip.min.js";
+//import {reader} from './ServerUnzip'
+
 
 export let ResultsDisposition = false;
+async function render(event){
+  const file = event.target.files[0];
+  const blobReader = new zip.BlobReader(file);
+  const zipReader = new zip.ZipReader (blobReader);
+  const entries = await zipReader.getEntries();
+  console.log(entries)
+  let urls = [];
+  
+  entries.forEach(async file => {
+    if (file.directory === false){ //aquilo que for uma diretoria não lhe interessa
+      const blob = await file.getData(new zip.BlobWriter(['image/jpeg', 'text/csv', 'text/html', 'application/json']));
 
+      const imageURL = URL.createObjectURL(blob);
+    
+      urls.push(imageURL);
+  }
+  });
+  await zipReader.close()
+  console.log(urls)
+  //return urls;
+}
 
 
 const Main = ({ outputsFiles, setOutputsFiles }) => {
 
-  let fileReader;
+  
+  //let fileReader;
 
-    const handleFolder = files => {
-      setOutputsFiles(files)
-      console.log(files)
-      const file = files.item(0);
-    };
-
+  /*const handleFolder = files => {
+    setOutputsFiles(files)
+    console.log(files)
+    const file = files.item(0);
+  };*/
+  const handleUploadClick = () => {
+    ResultsDisposition = true
+  }
+  const handleZipChange = (event) => {
+    console.log(render(event))
+  }
   return (
     <>
-        <Button
-          variant='contained'
-          color='secondary'
-          component="label"
-          ///ResultsDisposition = true // está a fazer com que o display resultados ative o subNav n sei porque* doen't make sense
-        >
-          Upload results folder
+      <Button
+        variant='contained'
+        color='secondary'
+        component="label"
+        onClick={handleUploadClick}
+      >
+        Upload results folder
           <input
-            type="file"
-            directory=""
-            webkitdirectory=""
-            onChange={ev => handleFolder(ev.target.files)}
-            hidden
-          />
-        </Button>
+          type="file"
+          accept='application/zip'
+          onChange={handleZipChange}
+          hidden
+        />
+      </Button>
 
-      <>{ ReactHtmlParser() }</>
+      <>{ReactHtmlParser()}</>
 
     </>
   )
