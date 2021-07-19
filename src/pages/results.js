4421e1c1d5a15e14d49a1b54cfaadb609776c5f5
iquasere <maildosequeira@gmail.com>
2021-07-19 14:56:04 +0100

    Started working on project page

--- src/pages/results.js
+++ src/pages/results.js
@@ -3,10 +3,8 @@ import { DashboardLayout } from '../components/Layout';
 import { Button, Toolbar, Typography } from "@material-ui/core";
 import * as zip from "@zip.js/zip.js";
 import $ from 'jquery'
-import * as ZIP from "@zip.js/zip.js/dist/zip-fs-full"
 import * as Papa from "papaparse"
 zip.configure({ useWebWorkers: false });
-
 
 const treatName = (name) =>{
   let resultingString = name.split('/')
@@ -19,8 +17,8 @@ export let ResultsDisposition = false;
 
 async function ObtainBlobArray(event){
   const file = event.target.files[0];
-  const blobReader = new ZIP.BlobReader(file);
-  const zipReader = new ZIP.ZipReader (blobReader);
+  const blobReader = new zip.BlobReader(file);
+  const zipReader = new zip.ZipReader (blobReader);
 
   const entries = await zipReader.getEntries();
 
@@ -38,40 +36,40 @@ async function ObtainBlobArray(event){
   for(let i = 0; i < entries.length; i++){
     if (entries[i].directory === false && entries[i].compressedSize !== 0){
       if(entries[i].filename.includes('Preprocess')){
-        const blobFastQC = await entries[i].getData(new ZIP.BlobWriter(['text/html']))
+        const blobFastQC = await entries[i].getData(new zip.BlobWriter(['text/html']))
         let fastQcName = treatName(entries[i].filename)
         FastQCReports.push({name: fastQcName, blob: blobFastQC})
       }
     if(entries[i].filename.includes('Annotation')){
-      const blobKronaPlots = await entries[i].getData(new ZIP.BlobWriter(['text/html']))
+      const blobKronaPlots = await entries[i].getData(new zip.BlobWriter(['text/html']))
       let kronaPlotsNames = treatName(entries[i].filename)
       KronaPlotsResults.push({name: kronaPlotsNames, blob: blobKronaPlots})
     }
     if(entries[i].filename.includes('Differential expression analysis')){
-      const blobHeatmaps = await entries[i].getData(new ZIP.BlobWriter(['image/jpeg']))
+      const blobHeatmaps = await entries[i].getData(new zip.BlobWriter(['image/jpeg']))
       let heatMapsNames = treatName(entries[i].filename)
       DifferentailExpressionResults.push({name: heatMapsNames, blob:blobHeatmaps})
 
     }
     if(entries[i].filename.includes('KEGGMaps')){
-      const blobKEGGMaps = await entries[i].getData(new ZIP.BlobWriter(['image/png']))
+      const blobKEGGMaps = await entries[i].getData(new zip.BlobWriter(['image/png']))
       let keggMaps = treatName(entries[i].filename)
       let number = KEGGMapsResults.length
       KEGGMapsResults.push({name:[number,keggMaps],blob:blobKEGGMaps})
 
     }
     if(entries[i].filename.includes('Assembly')){
-      const AssemblyReports = await entries[i].getData(new ZIP.BlobWriter(['text/tab-separated-values']))
+      const AssemblyReports = await entries[i].getData(new zip.BlobWriter(['text/tab-separated-values']))
       let assemblyName = treatName(entries[i].filename)
       Assembly.push({name: assemblyName, blob: AssemblyReports})
     }
     if(entries[i].filename.includes('Entry')){
-      const entryReport = await entries[i].getData(new ZIP.BlobWriter(['text/tab-separated-values']))
+      const entryReport = await entries[i].getData(new zip.BlobWriter(['text/tab-separated-values']))
       let entryName = treatName(entries[i].filename)
       entry.push({name: entryName, blob: entryReport})
     }
     if(entries[i].filename.includes('config')){
-      const config = await entries[i].getData(new ZIP.BlobWriter(['application/json']))
+      const config = await entries[i].getData(new zip.BlobWriter(['application/json']))
       const fileUrl = URL.createObjectURL(config)
       // eslint-disable-next-line no-loop-func
       $.getJSON(fileUrl, function(json){
@@ -79,17 +77,17 @@ async function ObtainBlobArray(event){
       })
     }
     if(entries[i].filename.includes('experiments')){
-      const exp = await entries[i].getData(new ZIP.BlobWriter(['text/tab-separated-values']))
+      const exp = await entries[i].getData(new zip.BlobWriter(['text/tab-separated-values']))
       exper = exp
     }
     if(entries[i].filename.includes('General')){
-      const genReport = await entries[i].getData(new ZIP.BlobWriter(['text/tab-separated-values']))
+      const genReport = await entries[i].getData(new zip.BlobWriter(['text/tab-separated-values']))
       let genName = treatName(entries[i].filename)
       general.push({name: genName, blob: genReport})
 
     }
     if(entries[i].filename.includes('Protein')){
-      const proteinReport = await entries[i].getData(new ZIP.BlobWriter(['text/tab-separated-values']))
+      const proteinReport = await entries[i].getData(new zip.BlobWriter(['text/tab-separated-values']))
       let protName = treatName(entries[i].filename)
       protein.push({name: protName, blob: proteinReport})
     }}
