b62056cc38aac65dc1e3bea76a5e3d293da8462a
iquasere <maildosequeira@gmail.com>
2023-05-22 14:27:22 +0100

    Working again

--- src/components/NavSidebar.js
+++ src/components/NavSidebar.js
@@ -2,122 +2,215 @@
 import { Navigation } from "react-minimal-side-navigation";
 import { useHistory, useLocation } from "react-router-dom";
 import Icon from "awesome-react-icons";
-import React, { useState } from "react";
+import React from "react";
 import { FaChartPie } from 'react-icons/fa';
-
+import { ResultsDisposition } from '../pages/results'
 import "react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css";
+
 
 export const NavSidebar = () => {
   const history = useHistory();
   const location = useLocation();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  return (
-    <>
-      {/* Sidebar Overlay */}
-      <div
-        onClick={() => setIsSidebarOpen(false)}
-        className={`fixed inset-0 z-20 block transition-opacity bg-black opacity-50 lg:hidden ${
-          isSidebarOpen ? "block" : "hidden"
-        }`}
-      />
-
-      <div>
-        <button
-          className="btn-menu"
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          type="button"
-        >
-          <Icon name="burger" className="w-6 h-6" />
-        </button>
-      </div>
-
-      {/* Sidebar */}
-      <div
-        className={`fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 ease-out transform translate-x-0 bg-white border-r-2 lg:translate-x-0 lg:static lg:inset-0 ${
-          isSidebarOpen ? "ease-out -translate-x-0" : "ease-in -translate-x-full"
-        }`}
-      >
-
-        <Navigation
-          activeItemId={location.pathname}
-          onSelect={({ itemId }) => {
-            history.push(itemId);
-          }}
-          items={[
-            {
-              title: "Home",
-              itemId: "/MOSGUITO/home",
-              elemBefore: () => <Icon name="coffee" />
-            },
-            {
-              title: "About",
-              itemId: "/MOSGUITO/about",
-              elemBefore: () => <Icon name="user" />,
-              subNav: [
-                {
-                  title: "Project",
-                  itemId: "/MOSGUITO/project"
-                },
-                {
-                  title: "Members",
-                  itemId: "/MOSGUITO/members"
-                }
-              ]
-            },
-            {
-              title: "Configuration",
-              itemId: "/MOSGUITO/config",
-              elemBefore: () => <Icon name="settings" />,
-              subNav: [
-                {
-                  title: "General configuration",
-                  itemId: "/MOSGUITO/general-configuration"
-                },
-                {
-                  title: "Experiments",
-                  itemId: "/MOSGUITO/experiments"
-                },
-                {
-                  title: "UniProt columns",
-                  itemId: "/MOSGUITO/uniprot-columns"
-                },
-                {
-                  title: "UniProt databases",
-                  itemId: "/MOSGUITO/uniprot-databases"
-                },
-                {
-                  title: "KEGG metabolic maps",
-                  itemId: "/MOSGUITO/keggmaps"
-                },
-                {
-                  title: "Proteomics configuration",
-                  itemId: "/MOSGUITO/proteomics-configuration"
-                }
-              ]
-            },
-            {
-                title: "Results",
-                itemId: "/MOSGUITO/results",
-                elemBefore: () => <FaChartPie />,
+  if (ResultsDisposition === false) {
+    return (
+      <>
+        {/* Sidebar */}
+        <div>
+          <Navigation
+            activeItemId={location.pathname}
+            onSelect={({ itemId }) => {
+              history.push(itemId);
+            }}
+            items={[
+              {
+                title: "Home",
+                itemId: "/MOSGUITO/home",
+                elemBefore: () => <Icon name="coffee" />
+              },
+              {
+                title: "About",
+                itemId: "/MOSGUITO/about",
+                elemBefore: () => <Icon name="user" />,
                 subNav: [
-                {
-                  title: "Load results",
-                  itemId: "/MOSGUITO/load-results"
-                },
-                {
-                  title: "FastQC reports",
-                  itemId: "/MOSGUITO/fastqc-reports"
-                },
-                {
-                  title: "Assembly QC",
-                  itemId: "/MOSGUITO/assembly-qc"
-                }
-              ]
+                  {
+                    title: "The MOSCA project",
+                    itemId: "/MOSGUITO/project"
+                  },
+                  {
+                    title: "Members",
+                    itemId: "/MOSGUITO/members"
+                  }
+                ]
+              },
+              {
+                title: "Configuration",
+                itemId: "/MOSGUITO/config",
+                elemBefore: () => <Icon name="settings" />,
+                subNav: [
+                  {
+                    title: "General configuration",
+                    itemId: "/MOSGUITO/general-configuration"
+                  },
+                  {
+                    title: "UniProt columns",
+                    itemId: "/MOSGUITO/uniprot-columns"
+                  },
+                  {
+                    title: "UniProt databases",
+                    itemId: "/MOSGUITO/uniprot-databases"
+                  },
+                  {
+                    title: "KEGG metabolic maps",
+                    itemId: "/MOSGUITO/keggmaps"
+                  },
+                  {
+                    title: "Experiments",
+                    itemId: "/MOSGUITO/experiments"
+                  }
+                ]
               }
-          ]}
-        />
-      </div>
-    </>
-  );
-};
+            ]}
+          />
+
+          <div>
+            <Navigation
+              activeItemId={location.pathname}
+              items={[
+                {
+                  title: "Results",
+                  itemId: "/MOSGUITO/results",
+                  elemBefore: () => <FaChartPie />
+                }
+              ]}
+              onSelect={({ itemId }) => {
+                history.push(itemId);
+              }}
+            />
+          </div>
+        </div>
+      </>
+    );
+  } else {
+
+    return (
+      <>
+        {/* Sidebar */}
+        <div>
+          <Navigation
+            activeItemId={location.pathname}
+            onSelect={({ itemId }) => {
+              history.push(itemId);
+            }}
+            items={[
+              {
+                title: "Home",
+                itemId: "/MOSGUITO/home",
+                elemBefore: () => <Icon name="coffee" />
+              },
+              {
+                title: "About",
+                itemId: "/MOSGUITO/about",
+                elemBefore: () => <Icon name="user" />,
+                subNav: [
+                  {
+                    title: "Project",
+                    itemId: "/MOSGUITO/project"
+                  },
+                  {
+                    title: "Members",
+                    itemId: "/MOSGUITO/members"
+                  }
+                ]
+              },
+              {
+                title: "Configuration",
+                itemId: "/MOSGUITO/config",
+                elemBefore: () => <Icon name="settings" />,
+                subNav: [
+                  {
+                    title: "General configuration",
+                    itemId: "/MOSGUITO/general-configuration"
+                  },
+                  {
+                    title: "Experiments",
+                    itemId: "/MOSGUITO/experiments"
+                  },
+                  {
+                    title: "UniProt columns",
+                    itemId: "/MOSGUITO/uniprot-columns"
+                  },
+                  {
+                    title: "UniProt databases",
+                    itemId: "/MOSGUITO/uniprot-databases"
+                  },
+                  {
+                    title: "KEGG metabolic maps",
+                    itemId: "/MOSGUITO/keggmaps"
+                  },
+                  {
+                    title: "Proteomics configuration",
+                    itemId: "/MOSGUITO/proteomics-configuration"
+                  }
+                ]
+              }
+            ]}
+          />
+
+          <div>
+            <Navigation
+              activeItemId={location.pathname}
+              items={[
+                {
+                  title: "Results",
+                  itemId: "/MOSGUITO/results",
+                  elemBefore: () => <FaChartPie />,
+                  subNav: [
+                    {
+                      title: "Load results",
+                      itemId: "/MOSGUITO/load-results"
+                    },
+                    {
+                      title: "FastQC reports",
+                      itemId: "/MOSGUITO/fastqc-reports"
+                    },
+                    {
+                      title: "Assembly QC",
+                      itemId: "/MOSGUITO/assembly-qc"
+                    },
+                    {
+                      title: 'Annotation Results',
+                      itemId: '/MOSGUITO/annotation-results'
+                    },
+                    {
+                      title: 'Differential Analysis',
+                      itemId: '/MOSGUITO/differential-analysis'
+                    },
+                    {
+                      title: 'KEGGmaps',
+                      itemId: '/MOSGUITO/keggmaps-results'
+                    },
+                    {
+                      title: 'EntryReports',
+                      itemId: '/MOSGUITO/entry-reports'
+                    },
+                    {
+                      title: 'GeneralReports',
+                      itemId: '/MOSGUITO/general-reports'
+                    },
+                    {
+                      title: 'ProteinReports',
+                      itemId: '/MOSGUITO/protein-reports'
+                    }
+                  ]
+                }
+              ]}
+              onSelect={({ itemId }) => {
+                history.push(itemId);
+              }}
+            />
+          </div>
+        </div>
+      </>
+    );
+  };
+}
